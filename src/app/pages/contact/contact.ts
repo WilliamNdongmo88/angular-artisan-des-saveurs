1dc63f2dbd719c6d994374b2d046e22e36682794
William Ndongmo <williamndongmo88@gmail.com>
2025-08-18 01:06:22 +0200

    MAJ de contact-2

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -48,6 +48,7 @@ export class ContactComponent implements OnInit {
     // Si l'utilisateur est connecté, pré-remplir le formulaire
     if (data && data.token) {
       this.isUserConnected = true;
+      this.isSubmitting = true;
       this.authService.extractUserFromToken(data.token); // Recupère les informations de l'utilisateur depuis le token
       const userData = this.authService.getUser();
       console.log('[ContactComponent] userData :: ', userData);
@@ -82,7 +83,6 @@ export class ContactComponent implements OnInit {
     console.log('isSubmitting:', this.isSubmitting);
     console.log('Form valid:', this.contactForm.valid);
     if (this.contactForm.valid && !this.isSubmitting) {
-      console.log('Form submitted:::');
       this.isSubmitting = true;
       this.submitMessage = '';
 
@@ -129,7 +129,55 @@ export class ContactComponent implements OnInit {
           }, 5000);
         }
       });
-    } else {
+    } if (this.isUserConnected && this.isSubmitting) {
+      this.isSubmitting = true;
+      this.submitMessage = '';
+
+      const formData: ContactForm = this.contactForm.value;
+
+      const contactRequest = {
+          subject: formData.subject,
+          message: formData.message
+      }
+      const contactRequests = [];
+      contactRequests.push(contactRequest)
+      const formDatas = {
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+          email: formData.email,
+          phone: formData.phone,
+          consent: formData.consent,
+          contactRequests: contactRequests
+      }
+
+      this.contactService.sendContactForm(formDatas).subscribe({
+        next: (response) => {
+          this.submitSuccess = true;
+          this.submitMessage = response.message;
+          this.contactForm.reset();
+          this.isSubmitting = false;
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+          
+          // Masquer le message après 5 secondes
+          setTimeout(() => {
+            this.submitMessage = '';
+            this.submitSuccess = false;
+          }, 5000);
+        },
+        error: (error) => {
+          this.submitSuccess = false;
+          this.submitMessage = 'Une erreur est survenue. Veuillez réessayer.';
+          this.isSubmitting = false;
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+          
+          // Masquer le message après 5 secondes
+          setTimeout(() => {
+            this.submitMessage = '';
+          }, 5000);
+        }
+      });
+    }
+    else {
       // Marquer tous les champs comme touchés pour afficher les erreurs
       Object.keys(this.contactForm.controls).forEach(key => {
         this.contactForm.get(key)?.markAsTouched();
