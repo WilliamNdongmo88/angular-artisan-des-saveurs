7bcc1033a0dae0fb2dae761e47775dd4e134f999
William Ndongmo <williamndongmo88@gmail.com>
2025-08-18 03:56:13 +0200

    Check de la validité du token-11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,6 +38,12 @@ export class AppComponent implements OnInit {
     console.log('AppComponent initialized this.isDashboard() :: ', this.isDashboard());
     console.log('[AppComponent] isAuthenticated ::', this.authService.isAuthenticated());
     if (this.authService.isAuthenticated() === false) {
+      console.log('[AppComponent] Token expiré ou non présent, redirection vers /login');
+      // Rediriger vers la page de connexion si l'utilisateur n'est pas authentifié
+      this.authService.user.set(null);
+      this.authService.isDashboard.set(false);
+      localStorage.removeItem('currentUser');
+      this.authService.logout();
       this.router.navigate(['/login']);
     } 
     //this.isDashboard = window.location.pathname.includes('/dashboard');
