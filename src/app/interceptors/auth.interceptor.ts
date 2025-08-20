943bb00be741de5d6cf408581be4b06aa8b6ce05
William Ndongmo <williamndongmo88@gmail.com>
2025-08-20 23:40:59 +0200

    Logique: généreration d'une URL de l'image  vers Nginx-7

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -5,15 +5,25 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   //console.log('[Interceptor] Token actuel dans localStorage <<-- Important:', currentUser.token);
 
   // N'ajoute pas d'en-tête Authorization pour la requête de connexion
+  // if (req.url.includes('/login') || req.url.includes('/register')) {
+  //   return next(req);
+  // }
   if (currentUser.token) {
+    // 👉 Vérifie si c'est un FormData (upload fichier)
+    const isFormData = req.body instanceof FormData;
     //console.log('authInterceptor token :', currentUser.token);
-    const authReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${currentUser.token.trim()}`,
-        'Content-Type': 'application/json'
-      }
-    });
-   //console.log('Intercepteur appliqué à :', authReq.url);
+    const headers: Record<string, string> = {
+      Authorization: `Bearer ${currentUser.token.trim()}`
+    };
+
+    if (!isFormData) {
+      // Seulement si ce n'est pas un upload
+      headers['Content-Type'] = 'application/json';
+    }
+
+    const authReq = req.clone({ setHeaders: headers });
+
+    //console.log('Intercepteur appliqué à :', authReq.url);
     //console.log('[Interceptor] Token ajouté:', currentUser.token);
     return next(authReq);
   }
