edc7bc0e49c53f76feff3b0208ec48524dea0a3c
William Ndongmo <williamndongmo88@gmail.com>
2025-08-27 01:19:03 +0200

    Logique: Implémentation du refresh token-16

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -34,12 +34,7 @@ export class AuthGuard implements CanActivate {
 
       if (hasRole && this.authService.isAuthenticated()) {
         return true; // accès autorisé
-      } else if(this.authService.isAuthenticated()){
-        // Rôle invalide
-        console.log('[AuthGuard] Rôle non autorisé, redirection');
-        this.router.navigate(['/']);
-        return false;
-      }else {
+      } else {
         // L'utilisateur n'est pas authentifié
         console.log('[AuthGuard] Token expiré, redirection vers /login');
         this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
