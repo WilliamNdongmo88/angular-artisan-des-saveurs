868363758088d6b3c57813276807ac7aaa7e4ea9
William Ndongmo <williamndongmo88@gmail.com>
2025-08-28 18:29:39 +0200

    Logique: Quantité de produit en kg ou g:6

--- src/app/models/product.models.ts
+++ src/app/models/product.models.ts
@@ -40,6 +40,8 @@ export interface ProductDto {
 export interface ProductItemDTO {
   id: number;
   quantity: number;
+  selectedUnit: string;
+  displayQuantity: number; 
   product: ProductDto;
 }
 
@@ -72,8 +74,5 @@ export interface ProductResponse {
   updatedAt: string;
   origin: string;
   preparation: string;
-  baseUnit: 'kg' | 'g' | 'piece'; // Unité de base du produit
-  pricePerBaseUnit: number; // Prix par unité de base (ex: prix par kg)
-  allowedUnits: ('kg' | 'g' | 'piece')[]; // Unités autorisées pour ce produit
 }
 
