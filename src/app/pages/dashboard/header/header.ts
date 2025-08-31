2e5a1634148db0b7cc476a832f6c52b942551bd9
William Ndongmo <williamndongmo88@gmail.com>
2025-08-31 23:56:02 +0200

    Logique: Gestion des commandes-22

--- src/app/pages/dashboard/header/header.ts
+++ src/app/pages/dashboard/header/header.ts
@@ -24,7 +24,7 @@ export class HeaderComponent implements OnInit {
   }
 
   goToDashboard() {
-    this.router.navigate(['/dashboard/products']);
+    this.router.navigate(['/dashboard']);
   }
 
   logout() {
